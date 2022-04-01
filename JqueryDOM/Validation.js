0d19302b54b7bbf7d879186764e6b33f9ac88a21
AbdoAitrais <90871152+AbdoAitrais@users.noreply.github.com>
2022-04-01 12:38:01 +0000

    Add files via upload

--- JqueryDOM/Validation.js
+++ JqueryDOM/Validation.js
@@ -1,39 +1,62 @@
-$(document).ready(function() {
-	// body...
-
-		function verifier(regularExp,inputValue){
-			if(regularExp.test(inputValue) && (inputValue.length > 1) && (inputValue.length < 30)){
-				$(".form-control").css({
+$(document).ready(function()
+{
+		function verifier(regularExp,inputValue,id)
+		{
+			if(regularExp.test(inputValue) && (inputValue.length > 1) && (inputValue.length < 30))
+			{
+				$(id).css(
+				{
 					"border-color" : "#54BE4A"
 				})
 				$(".span").hide();
 			}else{
-				$(".form-control").css({
+				$(id).css(
+				{
 					"border-color" : "red"
 				})
 				$(".span").show();
 			}
 		}
 	
-		$("#validate").click(function() {
+		$("#validate").click(function()
+		{
 			// body...
 			var regEx = /^[A-Z][a-zA-Z]+$/;
 			var name = $("#name").val();
-			var lastname = $("#lastname").val();
-			console.log(lastname);
-			console.log(regEx.test(name));
+			var lastname = $("#firstname").val();
+			
 
-			verifier(regEx,name);
-			if (lastname) {
-				verifier(regEx,lastname);
+			verifier(regEx,name,"#name");
+			if (lastname)
+			{
+				verifier(regEx,lastname,"#firstname");
 			}
 			
 		})
 
-		$("#add").click(function() {
+		$("#add").click(function()
+		{
+
 			$(".some").append("<div class = input-group><input type = text aria-label = firstname id = firstname class = form-control placeholder = Prenom ></div>");
 			$("#add").hide();
+			
 		})
+		var regEx = /^[A-Z][a-zA-Z]+$/;
+		var firstname = $("#firstname").val();
 
-	
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
